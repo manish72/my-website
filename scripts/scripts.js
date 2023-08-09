fa1cd5569de8bc09aaf1892f48823370e4e9dcc8
manish72 <manishguru72@gmail.com>
2023-08-09 17:02:27 +0530

    commented decoreatedExampleModals function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -96,7 +96,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
-  decorateExampleModals(main);
+  //decorateExampleModals(main);
 }
 
 /**
